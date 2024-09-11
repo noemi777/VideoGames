0ceed811731df5b1fd9d4454324f1ef87e16fbb6
NoemiC <143019471+noemi777@users.noreply.github.com>
2024-09-10 18:57:17 -0600

    feat: serialazer

--- videogames/serializers.py
+++ videogames/serializers.py
@@ -4,3 +4,8 @@ class VideoGamesSerializer(serializers.ModelSerializer):
     class Meta:
         model = VideoGames
         fields = '__all__'
+
+class VideoGamesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VideoGames
+        fields = ['id', 'name', 'release_date', 'game_category', 'played']
