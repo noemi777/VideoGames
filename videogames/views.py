0b22f8dbb1024b2c700b39937bf56833ebe6105f
NoemiC <143019471+noemi777@users.noreply.github.com>
2024-10-11 13:44:21 -0600

    fix: output errors

--- videogames/views.py
+++ videogames/views.py
@@ -39,7 +39,7 @@ class ReadGamesId(APIView):
             serializer = VideoGamesSerializer(game_obj)
             return Response(serializer.data)
         except:
-            return Response({'message':'No encontrado'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message':'Not found'},status=status.HTTP_400_BAD_REQUEST)
     
 class UpdateGame(APIView):
     permission_classes = (AllowAny, )
@@ -50,5 +50,5 @@ class UpdateGame(APIView):
             serializer = VideoGamesSerializer(game_obj)
             return Response(serializer.data)
         except:
-            return Response({'message':'Not Found'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message':'Not Found Element'},status=status.HTTP_400_BAD_REQUEST)
 
