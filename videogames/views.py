ce4b35e42eeebba00ae926a5b8b674a3e2dc9137
NoemiC <143019471+noemi777@users.noreply.github.com>
2025-03-10 17:21:45 -0600

    fix

--- videogames/views.py
+++ videogames/views.py
@@ -1,5 +1,3 @@
-from django.http import Http404
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
