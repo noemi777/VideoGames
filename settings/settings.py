5554582500376c5b0ed9f5987ee6e998419e791e
NoemiC <143019471+noemi777@users.noreply.github.com>
2025-03-10 17:24:41 -0600

    fix: format

--- settings/settings.py
+++ settings/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 import dj_database_url
 from pathlib import Path
 from decouple import config
